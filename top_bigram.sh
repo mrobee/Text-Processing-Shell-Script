3abd0b04b85f9d23b122666803a8aa7be2c3c898
tkid <tkid@tegalcyber.org>
2013-01-29 16:04:05 +0700

    Change mawk to awk

--- top_bigram.sh
+++ top_bigram.sh
@@ -4,7 +4,7 @@
 # Change awk to mawk for better performance.
 # add -s parameter to sort for allocate more memory usage.
 
-tr -sc 'A-Za-z0-9#_' '\012' < /dev/stdin | mawk '{print tolower($1)}' > all.words
+tr -sc 'A-Za-z0-9#_' '\012' < /dev/stdin | awk '{print tolower($1)}' > all.words
 tail -n +2 all.words > next.words
 paste all.words next.words > bigram.words
 cat all.words bigram.words | sort | uniq -c | sort -nr
